badebfa26907631147d3e9822deb66c4753651f0
Paulchen5 <lukas.23022005@gmail.com>
2025-01-28 09:49:21 +0000

    Update utilities/__init__.py
    Provide `Query`, `QueryManager`, `QueryExecutor`, `QuerySplitter` and `DataCleaner` objects directly

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,1 +1,2 @@
-from utilities.query_management import Query, QueryManager
+from utilities.query_management import Query, QueryExecutor, QueryManager, QuerySplitter
+from utilities.data_cleaner import DataCleaner
