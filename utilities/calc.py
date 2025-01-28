c8fa8287b09dc5a2e4d0f90bfded0266aeab62fc
Paulchen <lukas.23022005@gmail.com>
2025-01-28 17:00:46 +0100

    Update utilities/calc.py
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- utilities/calc.py
+++ utilities/calc.py
@@ -3,5 +3,5 @@ from datetime import timedelta as td
 
 
 def calculate_past_five_years_timestamp(start: dt) -> float:
-    past = start - td(weeks=260)  # 260 = 52 * 5 | past 5 years
+    past = start - td(weeks=52 * 5) # assume that one year has 52 weeks
     return past.timestamp()
