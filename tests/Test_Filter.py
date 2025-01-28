57ef645a0a6d9a3e5445a3234a50062ed8be5e65
Paulchen5 <lukas.23022005@gmail.com>
2025-01-28 09:55:10 +0000

    Update test cases | Add `tests/Test_Filter.py` for `utilities/data_filter.py`

---
+++ tests/Test_Filter.py
@@ -0,0 +1,89 @@
+import unittest
+
+from utilities.data_filter import create_time_ranges, remove_state_from_timestamp_value
+
+# TODO consider outsorcing into corresponding
+
+
+class TestFilter(unittest.TestCase):
+    def test_remove_state_from_timestamp_value_with_normal_input(self):
+        data = [[0.0, "1"], [13.0, "1"]]
+
+        expected_result = [0.0, 13]
+        result = remove_state_from_timestamp_value(data=data)
+
+        self.assertEqual(result, expected_result)
+
+    def test_remove_state_from_timestamp_value_with_empty_input(self):
+        data = []
+
+        expected_result = []
+        result = remove_state_from_timestamp_value(data=data)
+
+        self.assertEqual(result, expected_result)
+
+    def test_remove_state_from_timestamp_value_with_wrong_inputs(self):
+        data1 = "ABAP"
+        data2 = ["ABAP"]
+        data3 = [["ABAP"]]
+
+        # TODO good practice?
+        with self.assertRaises(TypeError):
+            remove_state_from_timestamp_value(data=data1)
+
+        with self.assertRaises(TypeError):
+            remove_state_from_timestamp_value(data=data2)
+
+        with self.assertRaises(TypeError):
+            remove_state_from_timestamp_value(data=data3)
+
+    def test_create_time_ranges_with_normal_input(self):
+        data = [0, 5, 10, 15, 35, 50, 55, 60, 65, 67, 68, 69, 73, 78, 83, 88, 90]
+        step = 5
+
+        expected_result = [(0, 15), (35, 0), (50, 15), (67, 0), (68, 0), (69, 0), (73, 15), (90, 0)]
+
+        result = create_time_ranges(data=data, step=step)
+
+        self.assertEqual(result, expected_result)
+
+    def test_create_time_ranges_with_empty_input(self):
+        data = []
+        step = 5
+
+        expected_result = []
+
+        result = create_time_ranges(data=data, step=step)
+
+        self.assertEqual(result, expected_result)
+
+    def test_create_time_ranges_with_continous_input(self):
+        data = [0, 5, 10, 15, 20, 25]
+        step = 5
+
+        expected_result = [(0, 25)]
+
+        result = create_time_ranges(data=data, step=step)
+
+        self.assertEqual(result, expected_result)
+
+    def test_create_time_ranges_with_one_input(self):
+        data = [77]
+        step = 5
+
+        expected_result = [(77, 0)]
+
+        result = create_time_ranges(data=data, step=step)
+
+        self.assertEqual(result, expected_result)
+
+    def test_create_time_ranges_with_wrong_input(self):
+        data = "ABAP"
+        step = 5
+
+        with self.assertRaises(TypeError):
+            create_time_ranges(data=data, step=step)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/Test_Query.py
+++ tests/Test_Query.py
@@ -1,23 +1,28 @@
 import unittest
+
 from datetime import datetime as dt
 from datetime import timedelta as td
 
 # from unittest.mock import MagicMock
+
 from utilities import Query
 
 API_ENDPOINT = "https://metrics-internal.qa-de-1.cloud.sap/api/v1/"
 
 
 class TestQuery(unittest.TestCase):
-    def test_get_alert_request_data(self):
+    def test_query_args_parsing(self):
         base_url = API_ENDPOINT
+
         now = dt.now()
         start = str(now.timestamp())
         end = str((now - td(hours=5)).timestamp())
         kwargs = {"params": {"step": "3600"}}
+
         query = Query(base_url=base_url, start=start, end=end, kwargs=kwargs)
-        result = query.get_alert_request_data(start=start, end=end)
+
         target = "query_range"
+
         params = {
             "query": "ALERTS",
             "dedup": "true",
@@ -29,8 +34,9 @@ class TestQuery(unittest.TestCase):
             "engine": "thanos",
             "analyze": "false",
         }
-        expected_result = {"target": target, "params": params}
-        self.assertEqual(result, expected_result)
+
+        self.assertEqual(query.params, params)
+        self.assertEqual(query.target, target)
 
 
 if __name__ == "__main__":
