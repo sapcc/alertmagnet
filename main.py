022d6ebb04b430b78f044fb25979e498c6b21cd7
Paulchen5 <lukas.23022005@gmail.com>
2025-02-10 13:17:07 +0000

    Fix https://github.com/sapcc/alertmagnet/pull/1#discussion_r1946542208

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def main(
     cert: str = None,
     timeout: int = 30,
     kwargs: dict = None,
-    storage_path: str = None,
+    directory_path: str = None,
     threshold: int = None,
 ):
     start = dt.now()
@@ -58,7 +58,7 @@ def main(
         kwargs = {}
 
     tm = ThreadManager(12)
-    qm = QueryManager(cert=cert, timeout=timeout, storage_path=storage_path, threshold=threshold, thread_manager=tm)
+    qm = QueryManager(cert=cert, timeout=timeout, storage_path=directory_path, threshold=threshold, thread_manager=tm)
 
     query = Query(base_url=api_endpoint)
 
