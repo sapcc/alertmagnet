27b9ac247d1fcabc5bb67e73f80ac5d607044a2c
Paulchen5 <lukas.23022005@gmail.com>
2025-03-03 13:02:19 +0000

    Update dependencies in `setup.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 setup(
     name="main",
     version="0.1.0",
-    py_modules=["main", "utilities"],
-    install_requires=["click", "requests"],
+    py_modules=["main", "analyzing", "extension", "filtering", "presenting", "querying", "utilities"],
+    install_requires=[
+        "click",
+        "pandas",
+        "prometheus-client",
+        "requests",
+    ],
 )
