fcbba6f52902fd216cc08cce38685f723eb8aa28
I588389 <lukas.michael@sap.com>
2025-03-04 10:42:06 +0000

    Resolve https://github.com/sapcc/alertmagnet/pull/6#discussion_r1978154472

--- filtering/data_cleaner.py
+++ filtering/data_cleaner.py
@@ -8,10 +8,6 @@ from filtering.data_filter import create_time_ranges
 
 logger = logging.getLogger("alertmagnet")
 
-logger = logging.getLogger("alertmagnet")
-
-logger = logging.getLogger("alertmagnet")
-
 
 class DataCleaner(object):
     def __init__(self):
